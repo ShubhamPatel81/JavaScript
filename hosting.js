5b551a6c416ddc2174a8f9b219e0a159d465f60e
Shubham Patel <shubhampatel872005@gmail.com>
2025-08-17 23:31:46 +0530

    first commit

---
+++ hosting.js
@@ -0,0 +1,25 @@
+// Hoisting is the default behaviour of javascript where all the variable and function declarations are moved on top./
+
+// https://www.interviewbit.com/javascript-interview-questions/
+
+hoistedVariable = 3;
+console.log(hoistedVariable); // outputs 3 even when the variable is declared after it is initialized	
+var hoistedVariable;
+
+hoistedFunction();  // Outputs " Hello world! " even when the function is declared after calling
+
+function hoistedFunction(){ 
+  console.log(" Hello world! ");
+} 
+
+// Hoisting takes place in the local scope as well
+function doSomething(){
+  x = 33;
+  console.log(x);
+  var x;
+} 
+
+"use strict";
+x = 23; // Gives an error since 'x' is not declared
+var x; 
+
